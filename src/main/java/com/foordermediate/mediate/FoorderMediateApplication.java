36af8b51f42b116afe2150a8d447cef3dc3e23d7
Sarthakdtu <sarthaksrivastavadtu712@gmail.com>
2022-01-07 16:57:27 +0530

    init

---
+++ src/main/java/com/foordermediate/mediate/FoorderMediateApplication.java
@@ -0,0 +1,13 @@
+package com.foordermediate.mediate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoorderMediateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoorderMediateApplication.class, args);
+	}
+
+}
